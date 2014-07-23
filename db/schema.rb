5bb6a7f31aba3fa7ea3f636626ca7a212755d44a
Raj Solanki <rajmahaj@gmail.com>
2014-07-23 14:05:31 -0600

    City CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140722012606) do
+ActiveRecord::Schema.define(version: 20140723195124) do
+
+  create_table "cities", force: true do |t|
+    t.string   "name"
+    t.string   "population"
+    t.string   "elevation"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", force: true do |t|
     t.string   "first_name"
